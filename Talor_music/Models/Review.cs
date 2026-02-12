7290f2ad239d328cb867d3e4f66ac770bc83a470
Talor8 <talor.sivan08@gmail.com>
2026-02-12 10:03:23 +0200

    התחלת הוספת דירוג

---
+++ Talor_music/Models/Review.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Talor_music.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; } // דירוג 1-5
+
+        
+        public string? Comment { get; set; } // תוכן התגובה
+
+        public DateTime DatePosted { get; set; } = DateTime.Now;
+
+        // קשר לשיר
+        public int SongID { get; set; }
+        public virtual Song? Song { get; set; }
+
+        // קשר ללקוח (מי כתב את הביקורת)
+        public string? CustomerID { get; set; }
+        public virtual Customer? Customer { get; set; }
+    }
+}
